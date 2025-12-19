08f8dfef36e5d3dd7879e31061c95457e5ebcc59
SangWon7242 <gotkdqja@gmail.com>
2025-12-19 18:32:10 +0900

    공통 헤더, 푸터 도입

---
+++ frontend/src/components/layout/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white p-4">
+      <div className="container mx-auto flex items-center justify-center">
+        <p>copyright &copy; 2025 AI Service Example. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ frontend/src/components/layout/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="bg-gray-800 text-white p-4 fixed inset-x-0 top-0 z-50">
+      <h1>Header</h1>
+    </header>
+  );
+};
+
+export default Header;
